b2674a7b1069734e28224a63718defda70c4d4fc
Viviane Wu <vivianewu92@MacBook-Pro-de-Viviane.local>
2017-04-04 22:41:16 +0800

    icons

--- src/App.js
+++ src/App.js
@@ -19,31 +19,23 @@ class App extends Component {
       <Provider store={ store }>
       <div>
         <nav>
-              <div className="nav-wrapper blue-grey darken-2">
-                  <a href="#" className="brand-logo">FooBar</a>
+          <div  className="nav-wrapper blue-grey darken-2">
+            <a href="#" className="brand-logo">FoodBar</a>
+            <div className="chip">
+          <img src="images/jlm.jpeg" alt="Contact Person" />
+          JLM
+            </div>
 
-                  <ul id="nav-mobile" className="right hide-on-med-and-down">
-                      <li><i className="material-icons">star f</i></li>
-                      <li>
-                          <a href="badges.html">Logout</a>
-                      </li>
-                  </ul>
-              </div>
-          </nav>
-          <nav>
-              <div className="nav-wrapper blue-grey darken-2">
-                  <form>
-                      <div className="input-field">
-                          <input id="search" type="search" required/>
-                          <label className="label-icon" htmlFor="search"><i className="material-icons">search</i></label>
-                          <i className="material-icons">close</i>
-                      </div>
-                  </form>
-              </div>
-          </nav>
+          </div>
+
+        </nav>
+
+        <div className="wrapper-icon">
+            <i className="medium material-icons">star</i>
+        </div>
+
 
         <div className="wrapper">
-
 
           <div className="container">
             <div className="row">
