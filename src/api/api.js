b3ba92d5e10910c670d7cec9ec06b0563ffe2740
Viviane Wu <vivianewu92@MacBook-Pro-de-Viviane.local>
2017-04-06 17:09:07 +0800

    added css

--- src/api/api.js
+++ src/api/api.js
@@ -4,12 +4,12 @@ module.exports = {
           function(ingredient, cb){
 
           const headers = new Headers({
-              "X-Mashape-Key": "XWkI3QrSiwmshqIW6tagnOvwpVdnp1VccZYjsnmytVAkNgFL85", //Viviane
-              // "X-Mashape-Key":"myag4vesVomshRjyCEbIzvRB9AvXp1KEaP9jsnXpyENrKtvP2u", //Chris
+              // "X-Mashape-Key": "XWkI3QrSiwmshqIW6tagnOvwpVdnp1VccZYjsnmytVAkNgFL85", //Viviane
+              "X-Mashape-Key":"myag4vesVomshRjyCEbIzvRB9AvXp1KEaP9jsnXpyENrKtvP2u", //Chris
               // "X-Yummly-App-Key":"2feabaa7be8cb35038e8faf080f3a2bf",
               "Content-Type": "application/json",
               "Accept": "application/json"});
-          const url = `https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=false&ingredients=${ingredient}&limitLicense=false&number=5&ranking=1`
+          const url = `https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=false&ingredients=${ingredient}&limitLicense=false&number=6&ranking=1`
           fetch(url, {headers})
               .then(function(response){
                 return response.text();
--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -7,31 +7,29 @@ class Recipe extends Component {
 
   handleClick = (e) => {
 
-  console.log("from test api", this.recipe.id.value)
 
-    // getLink(, function(all){
-    // console.log("from test api", e.target)
-    //
-    // })
+
+
   }
 
   render() {
 
     let { foodInstructions } = this.props;
-    console.log("this is this.props :", this.props);
-    console.log("this is foodInstructions: ", foodInstructions[0])
 
     let renderRecipes = () => {
 
       return foodInstructions.map((recipe) => {
             return (
 
-                  <div id={recipe.id}
-                  key={recipe.id} onClick={this.handleClick}>
-                      <h1>{recipe.title} </h1>
-                      <img src={recipe.image}/>
+              <div className="wrapper-recipe">
+                  <div key={recipe.id} onClick={this.handleClick}
+                   >
+                      <h1 className="title">{recipe.title} </h1>
+                      <img className="image" src={recipe.image}/>
                   </div>
+              </div>
             )
+
       })
     };
 
